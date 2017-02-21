e124a4eb7aa19f91aca471dd41a62e8efa54c4b8
Bram de Boer <bramdeboer@outlook.com>
2017-02-21 11:40:39 +0100

    Spring boot init

---
+++ src/main/java/com/s63b/CartrackingsimulationApplication.java
@@ -0,0 +1,12 @@
+package com.s63b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartrackingsimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartrackingsimulationApplication.class, args);
+	}
+}
